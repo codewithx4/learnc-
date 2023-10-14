d557220c2a52447306f1e6776ea60d87b95287e0
codewithx4 <113529442+codewithx4@users.noreply.github.com>
2023-10-14 19:35:02 +0530

    Update FirstProgram.c

--- FirstProgram.c
+++ FirstProgram.c
@@ -1,8 +1,8 @@
 /* Hello Guys, This Is Our First Program */
 
-#include <stdio.h>
+#include <stdio.h>  // Include the standard input/output library, which provides functions like printf.
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main() {  // The starting point of the program. Execution begins from here.
+    printf("Hello, World!\n");  // Print the string "Hello, World!" to the console.
+    return 0;  // Return 0 to the operating system, indicating successful program execution.
 }
